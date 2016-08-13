f63416df4e20166cab36e5a21ea5dded710f4e07
Maurits van der Schee <maurits@vdschee.nl>
2016-08-13 15:07:56 +0200

    Update auth.php

--- auth.php
+++ auth.php
@@ -157,3 +157,14 @@ class PHP_API_AUTH {
 		return false;
 	}
 }
+
+// uncomment the lines below when running in stand-alone mode:
+
+// for token+session based authentication (see "login_token.html" + "login_token.php"):
+
+// $auth = PHP_API_AUTH(array(
+// 	'secret'=>'someVeryLongPassPhrase',
+// 	'authenticator'=>function($user,$pass){ if ($user=='admin' && $pass=='admin') $_SESSION['user']=$user; }
+// ));
+// $auth->executeCommand();
+// if (empty($_SESSION['user'])) exit(403);
