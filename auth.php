e154f37ded07d1e897481909648bb09b5bbc4c66
Maurits van der Schee <maurits@vdschee.nl>
2016-08-13 14:36:32 +0200

    Update auth.php

--- auth.php
+++ auth.php
@@ -157,24 +157,3 @@ class PHP_API_AUTH {
 		return false;
 	}
 }
-
-// uncomment the lines below when running in stand-alone mode:
-
-// for token+session based authentication (see "login_token.html" + "login_token.php"):
-
-// $auth = PHP_API_AUTH(array(
-// 	'secret'=>'someVeryLongPassPhrase',
-// ));
-// $auth->executeCommand();
-// if (empty($_SESSION['user'])) exit(403);
-
-// for form+session based authentication (see "login.html"):
-
-// $auth = PHP_API_AUTH(array(
-// 	'authenticator'=>function($user,$pass){ if ($user=='admin' && $pass=='admin') $_SESSION['user']=$user; }
-// ));
-// $auth->executeCommand();
-// if (empty($_SESSION['user'])) exit(403);
-
-// include your api code here:
-echo 'Access granted!';
